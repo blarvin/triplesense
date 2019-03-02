cb0b53c118e9574bd6087ebdec84b88a1f5cb897
north <blarvinius@gmail.com>
2019-03-02 14:14:40 +0000

    initial localhost smoke test ...should fail

--- cypress/integration/examples/sample_spec.js
+++
@@ -1,7 +0,0 @@
-describe('Initial setup proof test:', function () {
-    it('visits the app and finds some HTML on a page:', function () {
-        cy.visit('localhost:3000')
-
-        cy.contains('pipe')
-    })
-})
